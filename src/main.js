028cd5143ca5670dc8b6288e60e9ba704864c4c1
Dahiana-moreno <dahian1507becerra@gmail.com>
2023-05-03 12:29:20 -0500

    actualizacion
    Se agrego una nueva vista.
    Extraer una porcion del array got.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import data from './data/got/got.js';
 let botonContinuar = document.getElementById("botonContinuar");
 vista1.style.display = "block";
 vista2.style.display = "none";
+vista3.style.display = "none";
 botonContinuar.style.display = "block";
 
 //evento cuando se presiona el boton continuar para cambiar a la segunda vista
@@ -11,9 +12,27 @@ botonContinuar.addEventListener("click", function () {
     document.getElementById("saludoJugador").innerHTML= "Bienvenid@ "+ nombreJugador + " elige una casa";
     vista1.style.display = "none";
     vista2.style.display = "block";
+    vista3.style.display = "none";
     botonContinuar.style.display = "block";
+
+
+    
+botonEnviar.addEventListener("click", function () {
+
+  vista1.style.display = "none";
+vista2.style.display = "none";
+vista3.style.display = "block";
+
+
+function filtroPorEdad(objeto) {
+  return objeto.id ==1;
+}
+const objetosFiltrados = data.got.filter(filtroPorEdad);
+
+console.log(objetosFiltrados);
+
+});
   });
 
 
-  
 
