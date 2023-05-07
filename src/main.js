226e61497c56639a8fb02f80e092dd93f0cae97e
Dahiana-moreno <dahian1507becerra@gmail.com>
2023-05-06 22:41:41 -0500

    vista3
    subos cambios de vista 3 y 2

--- src/main.js
+++ src/main.js
@@ -13,38 +13,58 @@ botonContinuar.addEventListener("click", function () {
     document.getElementById("saludoJugador").innerHTML= "Bienvenid@ "+ nombreJugador + " elige una casa";
     vista1.style.display = "none";
     vista2.style.display = "block";
-   
+    vista3.style.display = "none";
     botonContinuar.style.display = "block";
-      
-      let filtrarDatos = data.got.filter(casa => casa.family == "House Stark" || casa.family == "Stark");
-      console.log(filtrarDatos);
+
+  
+
+let botonEnviar = document.getElementById("botonEnviar");
+botonEnviar.addEventListener("click", function () {
+  vista1.style.display = "none";
+  vista2.style.display = "none";
+  vista3.style.display ="block";});
+
     
-      let name=filtrarDatos[0].fullName;
-      let titulo=filtrarDatos[0].title;
-      let familia=filtrarDatos[0].family;
-      let imagen=filtrarDatos[0].imageUrl;
-      let fechaNacimiento=filtrarDatos[0].born; 
-      let fechaMuerte=filtrarDatos[0].death;
+  let filtrarDatos = data.got.filter(casa => casa.family == "House Stark" || casa.family == "Stark");
+  console.log(filtrarDatos);
+  
+  let name=filtrarDatos[0].fullName;
+  let titulo=filtrarDatos[0].title;
+  let familia=filtrarDatos[0].family;
+  let imagen=filtrarDatos[0].imageUrl;
+  let fechaNacimiento=filtrarDatos[0].born; 
+  let fechaMuerte=filtrarDatos[0].death; 
+
   if (fechaMuerte=== undefined) {
     fechaMuerte="No aplica"
   }
-      document.getElementById("datos").innerHTM = name + "<br>"+ titulo +"<br>"+ familia +"<br>"+ imagen +"<br>"+ fechaNacimiento +"<br>"+fechaMuerte;
-    }
- 
-);
+
+  document.getElementById("datos").innerHTML= name + "<br>"+ titulo +"<br>"+ familia +"<br>"+ imagen +"<br>"+ fechaNacimiento +"<br>"+fechaMuerte;
+
+
+  let botonEnviar2 = document.getElementById("botonEnviar2");
+  botonEnviar2.addEventListener("click", function () {
+    vista1.style.display = "none";
+    vista2.style.display = "none";
+    vista3.style.display ="";});
 
 
 
-function filtroPor(objeto) {
-  return objeto.id ==1;
-}
-const objetosFiltrados = data.got.filter(filtroPor);
+    let home = document.getElementById("home");
+    home.addEventListener("click", function () {
+      vista1.style.display = "block";
+      vista2.style.display = "none";
+      vista3.style.display ="none";
+    });
 
-console.log(objetosFiltrados);
+    let retornar =document.getElementById("retornar");
+    retornar.addEventListener("click", function () {
+      vista2.style.display = "block";
+      vista3.style.display = "none";
+    });
 
 
-
-
+  });
 
 
 
