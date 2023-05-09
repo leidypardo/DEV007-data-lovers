35d8de188928ec8c7dfd9e4eb174954dd1635a8d
Dahiana-moreno <dahian1507becerra@gmail.com>
2023-05-09 11:47:11 -0500

    cambios vistas
    subo los cambios de vista 3 y barra navegacion.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-
 import data from './data/got/got.js';
 
 let botonContinuar = document.getElementById("botonContinuar");
@@ -18,53 +17,47 @@ botonContinuar.addEventListener("click", function () {
 
   
 
-let botonEnviar = document.getElementById("botonEnviar");
-botonEnviar.addEventListener("click", function () {
-  vista1.style.display = "none";
-  vista2.style.display = "none";
-  vista3.style.display ="block";});
-
-    
-  let filtrarDatos = data.got.filter(casa => casa.family == "House Stark" || casa.family == "Stark");
-  console.log(filtrarDatos);
+    let botonEnviar = document.querySelectorAll(".botonEnviar");
+    let cuandosehaceclick = function(){
+      let filtrarDatos = data.got.filter(casa => casa.family == "House Stark" || casa.family == "Stark");
+      console.log(filtrarDatos);
   
-  let name=filtrarDatos[0].fullName;
-  let titulo=filtrarDatos[0].title;
-  let familia=filtrarDatos[0].family;
-  let imagen=filtrarDatos[0].imageUrl;
-  let fechaNacimiento=filtrarDatos[0].born; 
-  let fechaMuerte=filtrarDatos[0].death; 
-
-  if (fechaMuerte=== undefined) {
-    fechaMuerte="No aplica"
-  }
-
-  document.getElementById("datos").innerHTML= name + "<br>"+ titulo +"<br>"+ familia +"<br>"+ imagen +"<br>"+ fechaNacimiento +"<br>"+fechaMuerte;
-
-
-  let botonEnviar2 = document.getElementById("botonEnviar2");
-  botonEnviar2.addEventListener("click", function () {
-    vista1.style.display = "none";
-    vista2.style.display = "none";
-    vista3.style.display ="";});
-
-
-
-    let home = document.getElementById("home");
-    home.addEventListener("click", function () {
-      vista1.style.display = "block";
+      let name=filtrarDatos[0].fullName;
+      let filtrarStark = data.got.filter(casa => casa.family == "House Stark" || casa.family == "Stark");
+      console.log(filtrarStark);
+  
+      let filtrarTargaryen = data.got.filter(casa => casa.family == "House Targaryen"|| casa.family == "Targaryan");
+      console.log(filtrarTargaryen); 
+  
+      let filtrarBaratheon = data.got.filter(casa => casa.family == "House Baratheon" || casa.family == "Baratheon");
+      console.log(filtrarBaratheon);
+  
+      let filtrarLannister = data.got.filter(casa => casa.family == "House Lannister" || casa.family == "House Lanister" || casa.family == "Lannister");
+      console.log(filtrarLannister);
+      
+      document.getElementById("datos").innerHTML= name;
+      vista1.style.display = "none";
       vista2.style.display = "none";
-      vista3.style.display ="none";
+      vista3.style.display ="block";
+      }
+      botonEnviar.forEach(boton => {
+        boton.addEventListener("click", cuandosehaceclick);
+         
+  
+      });
+      
+      let home = document.getElementById("home");
+      home.addEventListener("click", function () {
+        vista1.style.display = "block";
+        vista2.style.display = "none";
+        vista3.style.display ="none";
+      });
+  
+      let retornar =document.getElementById("retornar");
+      retornar.addEventListener("click", function () {
+        vista2.style.display = "block";
+        vista3.style.display = "none";
+      });
+  
+  
     });
-
-    let retornar =document.getElementById("retornar");
-    retornar.addEventListener("click", function () {
-      vista2.style.display = "block";
-      vista3.style.display = "none";
-    });
-
-
-  });
-
-
-
