8c09e3bc5e5f3b3ed2a6757038e1d3c6fa339e7e
Katherin palomino <122836306+leidypardo@users.noreply.github.com>
2023-05-09 11:07:20 -0500

    cambios

--- src/main.js
+++ src/main.js
@@ -16,10 +16,24 @@ botonContinuar.addEventListener("click", function () {
     vista3.style.display = "none";
     botonContinuar.style.display = "block";
       
-      let filtrarDatos = data.got.filter(casa => casa.family == "House Stark" || casa.family == "Stark");
-      console.log(filtrarDatos);
-    
-      let name=filtrarDatos[0].fullName;
+      let filtrarStark = data.got.filter(casa => casa.family == "House Stark" || casa.family == "Stark");
+      console.log(filtrarStark);
+      
+      let filtrarTargaryen = data.got.filter(casa => casa.family == "House Targaryen"|| casa.family == "Targaryan");
+      console.log(filtrarTargaryen); 
+      
+      let filtrarBaratheon = data.got.filter(casa => casa.family == "House Baratheon" || casa.family == "Baratheon");
+      console.log(filtrarBaratheon);
+      
+      let filtrarLannister = data.got.filter(casa => casa.family == "House Lannister" || casa.family == "House Lanister" || casa.family == "Lannister");
+      console.log(filtrarLannister);
+     
+      
+      //let filtrarTarly = data.got.filter(casa => casa.family == "House Tarly" || casa.family == "Stark");
+      //console.log(filtrarTarly); 
+      //funcionCiclo(filtrarStark)
+      
+     /* let name=filtrarDatos[0].fullName;
       let titulo=filtrarDatos[0].title;
       let familia=filtrarDatos[0].family;
       let imagen=filtrarDatos[0].imageUrl;
@@ -29,7 +43,13 @@ botonContinuar.addEventListener("click", function () {
     fechaMuerte="No aplica"
   }
       document.getElementById("datos").innerHTM = name + "<br>"+ titulo +"<br>"+ familia +"<br>"+ imagen +"<br>"+ fechaNacimiento +"<br>"+fechaMuerte;
-    }
+*/}
+
+
+  
+
+
+
  
 );
 
